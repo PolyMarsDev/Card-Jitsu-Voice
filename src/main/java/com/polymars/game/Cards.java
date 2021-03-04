e2b70595a4a4c755fc7424036cbd300a00b09570
BuildTools <marijnkneppers@gmail.com>
2021-03-04 11:33:35 +0100

    removed unused colors

--- src/main/java/com/polymars/game/Cards.java
+++ src/main/java/com/polymars/game/Cards.java
@@ -7,11 +7,7 @@ public class Cards {
     public static final int SNOW = 2;
     public static final int MIXED = 3;
 
-    public static final int RED = 0;
     public static final int ORANGE = 1;
-    public static final int YELLOW = 2;
-    public static final int GREEN = 3;
-    public static final int BLUE = 4;
     public static final int PURPLE = 5;
 
 }
